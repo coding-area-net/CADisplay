715e2e740d031528d77f6f539d0e863c9c87054e
KxmischesDomi <kxmischesdomi@gmail.com>
2022-04-05 13:22:38 +0200

    Delete common and bungee module

--- common/src/main/java/net/codingarea/common/Constants.java
+++
@@ -1,8 +0,0 @@
-package net.codingarea.common;
-
-public class Constants {
-
-    public static final String COMMON_MODEL_PACKAGE = "net.codingarea.common.models";
-
-    public static final String DATASTORE = "datastore";
-}
